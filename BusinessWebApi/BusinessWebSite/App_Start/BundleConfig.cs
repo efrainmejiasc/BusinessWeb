be4bc5c103cd157fdc686963d6ec0f572488de75
Willians Melendez <willian_melendez@hotmail.com>
2020-05-14 14:14:15 -0400

    adecuaciones en frontend

--- BusinessWebApi/BusinessWebSite/App_Start/BundleConfig.cs
+++ BusinessWebApi/BusinessWebSite/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace BusinessWebSite
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js", 
+                        "~/Content/js/global.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -23,7 +24,10 @@ namespace BusinessWebSite
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/bootstrap-theme.css",
+                      "~/Content/css/global.css",
+                      "~/Content/css/master.css",
                       "~/Content/site.css"));
         }
     }
